8573649f86ff77f052563bb4ffbdb1692ed7bf33
hosh-farms <admin@hoshfarms.com>
2025-08-29 04:09:17 +0530

    Update hosh_simulator_app.py

--- hosh_simulator_app.py
+++ hosh_simulator_app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1DOvh6xOx6_cgEYCSexLvc54twmIO6EKX
 """
 
-pip install streamlit
+#pip install streamlit
 
 import streamlit as st
 import pandas as pd
